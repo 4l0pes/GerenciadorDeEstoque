1008c7ba986ce99319763131d6f27249d6b900b4
Amanda Lopes <contato.4mandalopes@gmail.com>
2025-03-19 20:07:17 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import Entities.Products;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Products product = new Products();
+        System.out.println("Enter product data: ");
+        System.out.print("name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println();
+        System.out.println("product data: " + product);
+
+        System.out.println();
+        System.out.println("Insert the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.println("Insert the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+
+    }
+
+}
